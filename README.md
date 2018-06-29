b73ef51f86fd6da21559662d0cba89aecd81bdc0
williamspiro <35824747+williamspiro@users.noreply.github.com>
2018-06-29 00:41:51 -0400

    Blog Dawg

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # blogDawg
 Python tools for HubSpot blog objects
+
+__blogDawg.py__ - Turn any external blog into a HubSpot importable XML file  
+__blogFeaturedImageSoup.py__ - Soup featured images from an external blog, upload them to the File Manager, set them as featured for the posts respective HubSpot equivalent  
+
+## blogDawg.py
+A Python python to turn any external blog into an XML file which you can import into HubSpot    
+_REQUIRES_  
+[BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)  
+[lxml](https://lxml.de/)  
+
+_USAGE_  
+This Python python requires manually setting a few variables to make sure we can scrub external content, and get all the content and data we need to import a blog into HubSpot. It will grab post titles, urls, meta descriptions, authors, tags and post bodies, and turn them all into importable `<items>`  
+
+`blogRootUrl` - Root URL of the external blog    
+`blogPosts`- Python list of blog urls to import  
+`soup.` - There are 5 `soup.` statements which require a selector of the html elements(s) you are getting to grab content from. See the [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/) docs to learn more about modifieing these selectors depending on your needs  
+
+```
+$ python3 blogDawg.py
+```
+
+## blogFeaturedImageSoup.py
+A Python python to find the featured image on an external blog, upload it to the HubSpot File Manager, and then set the HubSpot hosted version of the posts' `featuredImage` with the newly uploaded File Manager asset  
+_REQUIRES_  
+[BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/)  
+[requests](http://docs.python-requests.org/en/master/)  
+
+_USAGE_  
+This Python python requires manually setting 4 variables: `accessToken`, `blogRootUrl`, `featuredImageSelector` & `postsToSoupScrubKitten`
+
+`accessToken` - An access_token for the portal you want to blogFeaturedImageSoup  
+`blogRootUrl`- The external blogs root url 
+`featuredImageSelector` - The CSS selector which select the external blogs featured image (ex. `.featured-image img`)  
+`postsToSoupScrubKitten` - A python list of the slugs of all of the external posts to soup (ex. `["slug/post/1", "slug/post/2", "slug/post/3"]`)  
+
+It is important that the post slugs of the external and HubSpot posts are the same. `blogRootUrl` + `postsToSoupScrubKitten[slugs]` should equal the actual URL of the posts. `slug`s should not start with a `/`, rather, `blogRootUrl` should end with a `/`
+
+```
+$ python3 blogFeaturedImageSoup.py
+```
---
+++ blogDawg.py
@@ -0,0 +1,113 @@
+from lxml import etree
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+# vars to set
+blogRootUrl = "https://coolwebsitedotcom.wordpress.com"
+blogPosts = ["https://coolwebsitedotcom.wordpress.com/2018/06/27/cool-post/", "https://coolwebsitedotcom.wordpress.com/2018/06/18/post-2-selected-same-fi-as-post-1/"]
+# TASK - CAN I SOMEHOW GET THE SELECTORS UP HERE?
+
+index = 1  
+authorList = []
+rss = etree.Element('rss')
+channel = etree.SubElement(rss, 'channel')
+rootLink = etree.SubElement(channel, 'link') 
+rootLink.text = blogRootUrl
+
+for post in blogPosts:
+    page = urlopen(post)
+    soup = BeautifulSoup(page, "html.parser")
+
+    # <content:encoded> value
+    postBody = soup.select(".entry-content")[0]
+    bodyStr = str(postBody)
+
+    # create <item>
+    item = etree.SubElement(channel, "item")
+
+    # set <title>
+    postTitle = soup.select(".entry-title")[0]
+    title = etree.SubElement(item, "title")
+    title.text = postTitle.text
+
+    # set <pubDate>
+    # TASK - FIGURE OUT HOW TO GET THE PUBLISH DATE WORKING WELL  
+    date = etree.SubElement(item, 'pubDate') 
+    date.text = 'Wed, 25 Apr 2018 13:19:35 +0000'
+
+    # set <link>
+    link = etree.SubElement(item, 'link') 
+    link.text = post
+
+    # set <wp:post_id>
+    postId = etree.SubElement(item, 'wppost_id') 
+    postId.text = str(index)
+
+    # set <wp:status>
+    status = etree.SubElement(item, 'wpstatus')
+    status.text = "publish"
+
+    # set <wp:post_type>
+    status = etree.SubElement(item, 'wppost_type')
+    status.text = "post"
+
+    # set <dc:creator> and create <wp:author>
+    # TASK - THIS FEELS REALLY SPECIFIC
+    postAuthor = soup.find('a', attrs={'rel':'author'})
+    author = etree.SubElement(item, 'dccreator')
+    author.text = postAuthor.text
+    if author.text not in authorList:
+        authorList.append(author.text)
+        authorPar = etree.SubElement(channel, 'wpauthor')
+        rootLink.addnext(authorPar)
+        authorParName = etree.SubElement(authorPar, 'wpauthor_display_name') 
+        authorParName.text = etree.CDATA(postAuthor.text)
+        authorParlog = etree.SubElement(authorPar, 'wpauthor_login') 
+        authorParlog.text = etree.CDATA(postAuthor.text)
+
+    # set <category>
+    postTags = soup.find_all('a', attrs={'rel':'category tag'})
+    for postTag in postTags:
+        category = etree.SubElement(item, 'category')
+        category.text = etree.CDATA(postTag.text)
+        category.set("domain", "category")
+        catSlug = category.text.replace(' ', '-')
+        category.set("nicename", catSlug)
+
+    # set <excerpt:encoded>
+    postMetaD = soup.find('meta', attrs={'name':'description'})
+    metaD = etree.SubElement(item, "excerptencoded")
+    metaD.text = postMetaD["content"]
+
+    # set <content:encoded>
+    content = etree.SubElement(item, 'contentencoded') 
+    content.text = etree.CDATA(bodyStr)
+
+    # TASK - FIGURE OUT COMMENTS
+
+    index += 1
+
+etree.ElementTree(rss).write("blog.xml", pretty_print=True, xml_declaration=True, encoding='UTF-8')
+
+def replace_all(text, dic):
+    for i, j in dic.items():
+        text = text.replace(i, j)
+    return text
+
+d = {
+    'contentencoded':'content:encoded',
+    'excerptencoded':'excerpt:encoded',
+    'wppost_id':'wp:post_id',
+    'wpstatus':'wp:status',
+    'wppost_type':'wp:post_type',
+    'dccreator':'dc:creator',
+    'wpauthor_display_name':'wp:author_display_name',
+    'wpauthor':'wp:author',
+    'wpauthor_login':'wp:author_login'
+    }
+
+with open('blog.xml', 'r') as file:
+    contents = file.read()
+new_contents = replace_all(contents, d)
+with open('blog.xml', 'w') as file:
+    file.write(new_contents)
