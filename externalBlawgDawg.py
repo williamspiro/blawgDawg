a14849f3011e7a63d629db564012eb8c9bb23f7b
williamspiro <35824747+williamspiro@users.noreply.github.com>
2018-06-29 12:21:59 -0400

    error handling and user agent

--- externalBlawgDawg.py
+++ externalBlawgDawg.py
@@ -1,6 +1,8 @@
 from lxml import etree
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+# from urllib.request import urlopen
+import urllib.request
+from urllib.error import HTTPError, URLError
 
 # vars to set
 blogRootUrl = 'https://coolwebsitedotcom.wordpress.com'
@@ -9,81 +11,89 @@ blogPosts = ['https://coolwebsitedotcom.wordpress.com/2018/06/27/cool-post/', 'h
 
 index = 1  
 authorList = []
+userAgent= {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'}
 rss = etree.Element('rss')
 channel = etree.SubElement(rss, 'channel')
 rootLink = etree.SubElement(channel, 'link') 
 rootLink.text = blogRootUrl
 
 for post in blogPosts:
-    page = urlopen(post)
-    soup = BeautifulSoup(page, 'html.parser')
-    
-    # create <item>
-    item = etree.SubElement(channel, 'item')
+    try:
+        req = urllib.request.Request(post, headers=userAgent)
+        page = urllib.request.urlopen(req)
+    except HTTPError as e:
+        print (f"hhhmmm, I had a hard time opeining {post}")
+    except URLError as e:
+        print (f"hhhmmm, I had a hard time opeining {post}")
+    else:
+        soup = BeautifulSoup(page, 'html.parser')
+        
+        # create <item>
+        item = etree.SubElement(channel, 'item')
 
-    # set <title>
-    postTitle = soup.find('title')
-    title = etree.SubElement(item, 'title')
-    title.text = postTitle.text
+        # set <title>
+        postTitle = soup.find('title')
+        title = etree.SubElement(item, 'title')
+        title.text = postTitle.text
 
-    # set <pubDate>
-    # TASK - FIGURE OUT HOW TO GET THE PUBLISH DATE WORKING WELL  
-    date = etree.SubElement(item, 'pubDate') 
-    date.text = 'Wed, 25 Apr 2018 13:19:35 +0000'
+        # set <pubDate>
+        # TASK - FIGURE OUT HOW TO GET THE PUBLISH DATE WORKING WELL  
+        date = etree.SubElement(item, 'pubDate') 
+        date.text = 'Wed, 25 Apr 2018 13:19:35 +0000'
 
-    # set <link>
-    link = etree.SubElement(item, 'link') 
-    link.text = post
+        # set <link>
+        link = etree.SubElement(item, 'link') 
+        link.text = post
 
-    # set <wp:post_id>
-    postId = etree.SubElement(item, 'wppost_id') 
-    postId.text = str(index)
+        # set <wp:post_id>
+        postId = etree.SubElement(item, 'wppost_id') 
+        postId.text = str(index)
 
-    # set <wp:status>
-    status = etree.SubElement(item, 'wpstatus')
-    status.text = 'publish'
+        # set <wp:status>
+        status = etree.SubElement(item, 'wpstatus')
+        status.text = 'publish'
 
-    # set <wp:post_type>
-    status = etree.SubElement(item, 'wppost_type')
-    status.text = 'post'
+        # set <wp:post_type>
+        status = etree.SubElement(item, 'wppost_type')
+        status.text = 'post'
 
-    # set <dc:creator> and create <wp:author>
-    # TASK - THIS FEELS REALLY SPECIFIC
-    postAuthor = soup.find('a', attrs={'rel':'author'})
-    author = etree.SubElement(item, 'dccreator')
-    author.text = postAuthor.text
-    if author.text not in authorList:
-        authorList.append(author.text)
-        authorPar = etree.SubElement(channel, 'wpauthor')
-        rootLink.addnext(authorPar)
-        authorParName = etree.SubElement(authorPar, 'wpauthor_display_name') 
-        authorParName.text = etree.CDATA(postAuthor.text)
-        authorParlog = etree.SubElement(authorPar, 'wpauthor_login') 
-        authorParlog.text = etree.CDATA(postAuthor.text)
+        # set <dc:creator> and create <wp:author>
+        # TASK - THIS FEELS REALLY SPECIFIC
+        postAuthor = soup.find('a', attrs={'rel':'author'})
+        author = etree.SubElement(item, 'dccreator')
+        author.text = postAuthor.text
+        if author.text not in authorList:
+            authorList.append(author.text)
+            authorPar = etree.SubElement(channel, 'wpauthor')
+            rootLink.addnext(authorPar)
+            authorParName = etree.SubElement(authorPar, 'wpauthor_display_name') 
+            authorParName.text = etree.CDATA(postAuthor.text)
+            authorParlog = etree.SubElement(authorPar, 'wpauthor_login') 
+            authorParlog.text = etree.CDATA(postAuthor.text)
 
-    # set <category>
-    postTags = soup.find_all('a', attrs={'rel':'category tag'})
-    for postTag in postTags:
-        category = etree.SubElement(item, 'category')
-        category.text = etree.CDATA(postTag.text)
-        category.set('domain', 'category')
-        catSlug = category.text.replace(' ', '-')
-        category.set('nicename', catSlug)
+        # set <category>
+        postTags = soup.find_all('a', attrs={'rel':'category tag'})
+        for postTag in postTags:
+            category = etree.SubElement(item, 'category')
+            category.text = etree.CDATA(postTag.text)
+            category.set('domain', 'category')
+            catSlug = category.text.replace(' ', '-')
+            category.set('nicename', catSlug)
 
-    # set <excerpt:encoded>
-    postMetaD = soup.find('meta', attrs={'name':'description'})
-    metaD = etree.SubElement(item, 'excerptencoded')
-    metaD.text = postMetaD['content']
+        # set <excerpt:encoded>
+        postMetaD = soup.find('meta', attrs={'name':'description'})
+        metaD = etree.SubElement(item, 'excerptencoded')
+        metaD.text = postMetaD['content']
 
-    # set <content:encoded> 
-    postBody = soup.select('.entry-content')[0]
-    bodyStr = str(postBody)
-    content = etree.SubElement(item, 'contentencoded') 
-    content.text = etree.CDATA(bodyStr)
+        # set <content:encoded> 
+        postBody = soup.select('.entry-content')[0]
+        bodyStr = str(postBody)
+        content = etree.SubElement(item, 'contentencoded') 
+        content.text = etree.CDATA(bodyStr)
 
-    # TASK - FIGURE OUT COMMENTS
+        # TASK - FIGURE OUT COMMENTS
 
-    index += 1
+        index += 1
 
 etree.ElementTree(rss).write('blog.xml', pretty_print=True, xml_declaration=True, encoding='UTF-8')
 
